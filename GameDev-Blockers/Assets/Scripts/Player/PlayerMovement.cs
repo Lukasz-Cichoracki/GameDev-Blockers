28ff746ed029ec5f1589818f30a5c99f8b01779c
Lukasz-Cichoracki <bukasz1500@gmail.com>
2023-01-16 22:01:25 +0100

    Updating functionalities
    Working on camera movement and player movement when stepping on log

--- GameDev-Blockers/Assets/Scripts/Player/PlayerMovement.cs
+++ GameDev-Blockers/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private GameObject _log;
+    private bool _isMoving=false;
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
@@ -14,6 +19,22 @@ public class PlayerMovement : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.D))
             transform.position += new Vector3(1f, 0f, 0f);
 
-    }
+      
 
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.tag == "log")
+        {
+           _log = collision.gameObject;
+            Debug.Log("dupa");
+            _isMoving = true;
+        }
+     
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "log")
+            _isMoving = false;
+    }
 }
