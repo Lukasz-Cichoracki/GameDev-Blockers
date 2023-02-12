717dd40c1b513903952de2b9f3723aed70982cd1
Lukasz-Cichoracki <bukasz1500@gmail.com>
2023-02-12 21:02:03 +0100

    Added zombies
    -Added zombies
    -Updated map visuals
    -Optimised code for log and zombies via Game Manager

--- GameDev-Blockers/Assets/Scripts/GameManager/GameManager.cs
+++ GameDev-Blockers/Assets/Scripts/GameManager/GameManager.cs
@@ -4,11 +4,31 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static Vector3 toStartPosition;
+    public static Vector3 move;
+    public static float teleportPos = 6.75f;
+    
+    private void Awake()
+    {
+        toStartPosition = new Vector3(-14f, 0f, 0f);
+        move = new Vector3(0.25f, 0f, 0f);
+    }
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(0);
         }
+    }
+
+    public static void MoveGameObject(Transform objPosition,int direction)
+    {
+        if (objPosition.position.x == direction * teleportPos)
+        {
+            objPosition.position += toStartPosition * direction;
+            return;
+        }
+        objPosition.position += move * direction;
     }
 }
