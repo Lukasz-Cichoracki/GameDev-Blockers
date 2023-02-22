02f116941cea3e4c554f1725d173e4168730f108
Lukasz-Cichoracki <bukasz1500@gmail.com>
2023-02-22 09:48:00 +0100

    Added checkpoint text fading

--- GameDev-Blockers/Assets/Scripts/Checkpoints/ChecpointsScript.cs
+++ GameDev-Blockers/Assets/Scripts/Checkpoints/ChecpointsScript.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ChecpointsScript : MonoBehaviour
 {
+    private GameObject fadeObject;
+    private TextMeshProUGUI fadeText;
+    private void Start()
+    {
+        fadeObject = GameObject.Find("CheckpointText"); 
+        fadeText = fadeObject.GetComponent<TextMeshProUGUI>();
+    }
+    private IEnumerator Fade()
+    {
+        if (fadeText != null)
+        {
+            Color currentColor = fadeText.color;
+            while (fadeText.color.a < 1)
+            {
+                yield return new WaitForSeconds(0.04f);
+                currentColor.a += 0.05f;
+                fadeText.color = currentColor;
+            }
+            while (fadeText.color.a > 0)
+            {
+                yield return new WaitForSeconds(0.04f);
+                currentColor.a -= 0.05f;
+                fadeText.color = currentColor;
+            }
+            currentColor.a = 0;
+            fadeText.color = currentColor;
+            Destroy(this.gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "player")
         {
             PlayerMovement.playerRespawn = this.transform.position;
-            Destroy(this.gameObject);
+            StartCoroutine("Fade");           
         }
     }
 }
